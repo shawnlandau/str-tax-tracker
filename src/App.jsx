351e00a0834bc5cb2e6069ad101885121dc4dcb1
shawnlandau <shawnjl@outlook.com>
2025-07-10 14:05:39 -0400

    Fix routing issues for Netlify deployment - Remove duplicate Router wrapper and add _redirects file for client-side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import Properties from './pages/Properties'
@@ -8,16 +8,14 @@ import TaxForms from './pages/TaxForms'
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/properties" element={<Properties />} />
-          <Route path="/depreciation" element={<Depreciation />} />
-          <Route path="/tax-forms" element={<TaxForms />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/depreciation" element={<Depreciation />} />
+        <Route path="/tax-forms" element={<TaxForms />} />
+      </Routes>
+    </Layout>
   )
 }
 
